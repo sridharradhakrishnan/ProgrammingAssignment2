f50dafe4a5ce11ba2be12d84161f27a05e803245
Sridhar Radhakrishnan <sridhar@ou.edu>
2014-05-24 12:45:19 -0500

    Code completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## The purpose of this project is to learn how you can cache results of computations
+## This will speed up if the same object is used time after time
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) inverse <<- solve
+  getinverse <- function() inverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
+## Given a non-singular matrix the cacheSovle will find the inverse if not already determined
+## Otherwise it will calculate and store it the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+}
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
